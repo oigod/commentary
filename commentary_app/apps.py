3abe88d68fdd65eb868bf607ec390afaaf7f4c6a
oithegod <serejathegod@gmail.com>
2023-05-19 21:22:05 +0300

    Added signals to app.py

--- commentary_app/apps.py
+++ commentary_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CommentaryAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "commentary_app"
+
+    def ready(self):
+        import commentary_app.signals

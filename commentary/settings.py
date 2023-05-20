84c12d4a4dd656e1b208b7bb9d354de56e6ac31c
oithegod <serejathegod@gmail.com>
2023-05-20 15:26:16 +0300

    fix deploy error

--- commentary/settings.py
+++ commentary/settings.py
@@ -17,7 +17,12 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+
+DOTENV_PATH = BASE_DIR / ".env"
+if os.path.exists(DOTENV_PATH):
+    from dotenv import load_dotenv
+
+    load_dotenv(DOTENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/

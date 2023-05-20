5a3babb74c6ae3d9b79727847983c99eca8a38c1
oithegod <serejathegod@gmail.com>
2023-05-20 16:25:20 +0300

    added domen to allowed hosts

--- commentary/settings.py
+++ commentary/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"] == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "comentar.life"]
 
 # Application definition
 
